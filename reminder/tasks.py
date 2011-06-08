b1bf12fa9cb0a5476baec551be9ce2c3c8388510
Slawa Rokicki <slawa.rokicki@gmail.com>
2011-06-08 12:10:46 +0000

    Event schedule and broadcast working

--- reminder/tasks.py
+++ reminder/tasks.py
@@ -2,13 +2,14 @@
 # vim: ai ts=4 sts=4 et sw=4
 
 #from rapidsms.router import router
-import logging
+#import logging
 #from reminder.models import Subject
 #from reminder.utils import MSGS
 #from django.db.models.query import Q
 
-def broadcast(router, arg):
-    logging.debug('>>>>>>>>>>>>>>>>>  broadcasting')
+def broadcast(router):
+#    logging.debug('>>>>>>>>>>>>>>>>>  broadcasting')
+    print '>>>>>>>>> broadcastingadg;fklas'
 #    backend = router.get_backend('gsm')
 #    subjects = Subject.objects.filter(Q(messages_left__gt=0)|
 #                                      Q(active=True))
